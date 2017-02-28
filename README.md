fcae1be5aec76c94f37e4e4fe5a9b6770dac5c57
Chris Wright <daverandom@php.net>
2017-02-28 17:09:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Admin front-end for room11 lxr
---
+++ src/launcher.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+namespace Room11\Lxr\Admin;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+header('Content-Type: text/plain');
+echo "There's nothing here yet. Blame @JayIsTooCommon.";
