6a1551f9920fa4fe5ca850ee8fe89a8f3c391e10
Chris Wright <daverandom@php.net>
2017-02-28 18:10:28 +0000

    Add nginx config and HTMLify holding page

--- src/launcher.php
+++ src/launcher.php
@@ -4,5 +4,18 @@ namespace Room11\Lxr\Admin;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-header('Content-Type: text/plain');
-echo "There's nothing here yet. Blame @JayIsTooCommon.";
+header('Content-Type: text/html; charset=UTF-8');
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>It's @JayIsTooCommon's fault</title>
+  </head>
+  <body>
+    <h1>There's nothing here yet</h1>
+    <h2>Blame @JayIsTooCommon</h2>
+    <img alt="Jaystin Bieber" src="/jaystin-bieber.jpg">
+  </body>
+</html>
